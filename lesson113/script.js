4ab039df27a77d39f1b9a18e1a4f7fd6edc6c081
Gypsophiila <ivanmainac@gmail.com>
2025-02-12 22:06:15 +0300

    Add files via upload

---
+++ lesson113/script.js
@@ -0,0 +1,29 @@
+const buttonOpen = document.querySelector('.button-open')
+const modal = document.querySelector('.modal')
+const body = document.body
+
+
+
+const closeModul = () => {
+    modal.classList.remove('modal--open')
+    body.classList.remove('body--fixed')
+}
+
+buttonOpen.addEventListener('click', () => {
+    modal.classList.add('modal--open')
+    body.classList.add('body--fixed')
+})
+
+modal.addEventListener('click', (event) => {
+    const target = event.target
+    
+    if (target && target.classList.contains('button-close')) {
+        closeModul()
+    }
+})
+
+document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+        closeModul()
+    }
+})
